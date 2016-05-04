ea727f537fab712b4a4d681162276bfb7cb41493
Max Anderson <cctpmax@gmail.com>
2016-05-04 14:23:23 -0400

    Added more functionality and fixed module selection
    Made the generator add START to the beginning of Commands_Random.txt instead of having the user do it automatically. Also made the module selection actually work by adding -Module $Modules to the get-command command.

--- Generate Random List of Commands.ps1
+++ Generate Random List of Commands.ps1
@@ -1,15 +1,14 @@
-﻿#Add which modules you'd like to be emailed about
+#Add which modules you'd like to be emailed about
 $Modules = @(
 "ActiveDirectory"
 "Hyper-V"
 )
 
-
-[System.Collections.ArrayList]$cmdlets = get-command | Select-Object Name
-
-
+[System.Collections.ArrayList]$cmdlets = get-command -Module $Modules| Select-Object Name
 $cmdlets_new = $cmdlets | Where-Object {$_.name -notmatch '[A-Z]:' -and $_.name -notlike "cd*"}
-
 $cmdlets_new = $cmdlets_new.Name
+$cmdlets_new = $cmdlets_new | Sort-Object {Get-Random}
 
-$cmdlets_new | Sort-Object {Get-Random} | Out-File Commands_Random.txt
+New-Item -Path Commands_Random.txt -Force
+Add-Content -Path .\Commands_Random.txt -Value START
+Add-Content -Path .\Commands_Random.txt -Value $cmdlets_new
